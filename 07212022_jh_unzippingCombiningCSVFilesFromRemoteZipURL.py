90e1e7f151dd633396315cd11a471e63a370df11
lightsavers <webanalyst@gmail.com>
2022-07-21 15:53:19 -0400

    Added comment lines for easy code file navigation and future reference

--- 07212022_jh_unzippingCombiningCSVFilesFromRemoteZipURL.py
+++ 07212022_jh_unzippingCombiningCSVFilesFromRemoteZipURL.py
@@ -1,17 +1,32 @@
+''' **********************
+*
+* Author: lightsavers
+* Created: 07212022
+*
+***********************'''
+
 import os
 from io import BytesIO
 from zipfile import ZipFile
 import pandas as pd
 import requests
 
+# Sample remote URL ZIp file
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/IndoorMovement.zip"
+
+#store URL connection in variable to extract contents next
 content = requests.get(url)
+
+#use zipfile and BYtesIO to store contents in zipfile object
 archive = ZipFile(BytesIO(content.content))
 
+#iterate thru files inside zip and identify folder 'dataset'
 files = [i for i in archive.namelist() if i.startswith('dataset') ]
 
+# declare empty list to store future dataframes
 dfList = []
 
+# iterate thru the files inside the 'dataset' directory and append them to list
 for file in files:
     if file.endswith('.csv') and file != 'MovementAAL_target.csv':
             #store each csv file as dataframe
@@ -23,57 +38,18 @@ for file in files:
 
 #merge the list of dataframes into a single dataframe (this assumes all dataframes have the same columns)
 df = pd.concat(dfList)
+
+#print top 25 rows to console or terminal
 print(df.head(25))
 
+#close archive object connection
 archive.close()
 
+#list current working directory
 cwd = os.getcwd()
+
+#store path of current working directory in variable
 path = cwd
 
+#save file dataframe to csv file
 df.to_csv("testing2.csv", sep=',', encoding='utf-8')
-
-# *****************************************************************#
-# everything below this line is experimental
-
-'''
-for datadirectory in archive.namelist():
-    if datadirectory.startswith('dataset/'):
-        archive.extract(datadirectory)
-'''
-
-'''
-    if datadirectory.startswith('dataset/'):
-        for sheet in archive.namelist():
-            tempdf = pd.read_csv(sheet, index_col=0, header=0)
-            data.append(tempdf)
-'''
-
-#df = pd.concat(data, axis=1)
-
-#print(df)
-
-
-'''
-
-dfs += [pd.read_csv(zf.open('*.csv')]
-
-dfs = []
-
-s_dir = lambda zipinfo: zipinfo.filename.endswith('/')
-
-with zf as z:    
-    for filename in z.namelist():
-        file_info = z.getinfo(filename)
-        if file_info.is_dir():
-            # read the file
-            if filename.endswith('.csv'):
-                for line in z.open(filename):
-                    #df = pd.read_csv(zf.open(line))
-                    #print(line.decode('utf-8'))
-                    
-                    #zip_file = os.path.join(zf, filename)
-                    #zf = zipfile.ZipFile(zip_file)
-                    dfs += [pd.read_csv(z.open(filename), header=None, sep=";", encoding='latin1') for f in z.namelist()]
-
-df = pd.concat(dfs,ignore_index=True)
-'''
